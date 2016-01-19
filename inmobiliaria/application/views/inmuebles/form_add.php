a720f48fce29cdf3c9dcced0dd7d4cdac7f187af
SMartinMedina <smartinmedina@gmail.com>
2016-01-19 11:02:47 -0300

    Improving Form Add
    A

--- inmobiliaria/application/views/inmuebles/form_add.php
+++ inmobiliaria/application/views/inmuebles/form_add.php
@@ -18,97 +18,134 @@
 			}
 		?>
 		<?php echo form_open('inmuebles/addInmueble',array('id'=>"form_add")); ?>
+
+
 		<div class="widget-content">
 			<div class="nonboxy-widget">
 				<div class="widget-head">
-					<h5>Agregar Inmueble</h5>
+					<h5>Detalles</h5>
 				</div>
-				<div class="control-group">
-					<label class="control-label" for="typehead">Tipo de Inmueble</label>
-					<div class="controls">
-						<div>
-							<select id = "id_tinmueble" name = "id_tinmueble" autocomplete = "off">
-								<option value = "" selected = 'selected'>Seleccione</option>
-							<?php
-								foreach($tinmuebles as $ti){
-							?>
-								<option value = "<?php echo $ti->id?>"><?php echo $ti->nombre; ?></option>
-							<?php
-								}
-							?>
-							</select>
+
+				<div class = "row">
+					<div class = "span3">
+						<div class="control-group">
+							<label class="control-label" for="typehead">Tipo de Inmueble</label>
+							<div class="controls">
+								<div>
+									<select id = "id_tinmueble" name = "id_tinmueble" autocomplete = "off">
+										<option value = "" selected = 'selected'>Seleccione</option>
+									<?php
+										foreach($tinmuebles as $ti){
+									?>
+										<option value = "<?php echo $ti->id?>"><?php echo $ti->nombre; ?></option>
+									<?php
+										}
+									?>
+									</select>
+								</div>
+							</div>
 						</div>
 					</div>
-				</div>
-				<div class="control-group">
-					<label class="control-label" for="typehead">Operacion</label>
-					<div class="controls">
-						<div>
-							<select id = "id_operacion" name = "id_operacion" autocomplete = "off">
-								<option value = "" selected = 'selected'>Seleccione</option>
-							<?php
-								foreach($operaciones as $o){
-							?>
-								<option value = "<?php echo $o->id?>"><?php echo $o->nombre; ?></option>
-							<?php
-								}
-							?>
-							</select>
+					<div class = "span3">
+						<div class="control-group">
+							<label class="control-label" for="typehead">Operacion</label>
+							<div class="controls">
+								<div>
+									<select id = "id_operacion" name = "id_operacion" autocomplete = "off">
+										<option value = "" selected = 'selected'>Seleccione</option>
+									<?php
+										foreach($operaciones as $o){
+									?>
+										<option value = "<?php echo $o->id?>"><?php echo $o->nombre; ?></option>
+									<?php
+										}
+									?>
+									</select>
+								</div>
+							</div>
 						</div>
 					</div>
-				</div>
-				<div class="control-group">
-					<label class="control-label" for="typehead">Estado del Inmueble</label>
-					<div class="controls">
-						<div>
-							<select id = "estado_inmueble" name = "estado_inmueble" autocomplete = "off">
-								<option value = "" selected = 'selected'>Seleccione</option>
-							<?php
-								foreach($estados_inmueble as $ei){
-							?>
-								<option value = "<?php echo $ei->id?>" ><?php echo $ei->nombre; ?></option>
-							<?php
-								}
-							?>
-							</select>
+					<div class = "span3">
+						<div class="control-group">
+							<label class="control-label" for="typehead">Estado del Inmueble</label>
+							<div class="controls">
+								<div>
+									<select id = "estado_inmueble" name = "estado_inmueble" autocomplete = "off">
+										<option value = "" selected = 'selected'>Seleccione</option>
+									<?php
+										foreach($estados_inmueble as $ei){
+									?>
+										<option value = "<?php echo $ei->id?>" ><?php echo $ei->nombre; ?></option>
+									<?php
+										}
+									?>
+									</select>
+								</div>
+							</div>
 						</div>
 					</div>
-				</div>
-				<div class="control-group">
-					<label class="control-label" for="typehead">Ubicacion Geografica</label>
-					<div class="controls">
-						<div>
-							<select id = "id_provincia" name = "id_provincia" autocomplete = "off">
-								<option value = "" selected = 'selected'>Provincia</option>
-							<?php
-								foreach($provincias as $p){
-							?>
-								<option value = "<?php echo $p->id?>"><?php echo $p->nombre; ?></option>
-							<?php
-								}
-							?>
-							</select>
-							<input type = "hidden" id = "provincia_text" name = "provincia_text" value = ""/>
-							&nbsp;
-							<select id = "id_departamento" name = "id_departamento" autocomplete = "off">
-								<option value = "">Departamento</option>
-							</select>
-							<input type = "hidden" id = "departamento_text" name = "departamento_text" value = ""/>
-							&nbsp;
-							<select id = "id_localidad" name = "id_localidad" autocomplete = "off">
-								<option value = "">Localidad/Barrio</option>
-							</select>
-							<input type = "hidden" id = "localidad_text" name = "localidad_text" value = ""/>
-						</div>
+					<div class = "span3">
 					</div>
 				</div>
-				<div class="control-group">
-					<label class="control-label" for="typehead">Direccion</label>
-					<div class="controls">
-						<div>
-							<input type="text" class="span2" placeholder = "Calle" id="calle" name ="calle" value = "" maxlength= "50">
-							<input type="text" class="span1" placeholder = "Altura" id="altura" name ="altura" value = "" maxlength= "5">
-							<select id = "piso" name="piso" class="span1">
+				
+				<div class = "row">
+					<div class = "span12">
+						<label class="control-label" for="typehead">Ubicacion Geografica</label>
+					</div>
+				</div>
+				<div class = "row">
+					<div class = "span3">
+						<div class="control-group">
+							<div class="controls">
+								<div>
+									<select id = "id_provincia" name = "id_provincia" autocomplete = "off">
+										<option value = "" selected = 'selected'>Provincia</option>
+									<?php
+										foreach($provincias as $p){
+									?>
+										<option value = "<?php echo $p->id?>"><?php echo $p->nombre; ?></option>
+									<?php
+										}
+									?>
+									</select>
+									<input type = "hidden" id = "provincia_text" name = "provincia_text" value = ""/>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class = "span3">
+						<select id = "id_departamento" name = "id_departamento" autocomplete = "off">
+							<option value = "">Departamento</option>
+						</select>
+						<input type = "hidden" id = "departamento_text" name = "departamento_text" value = ""/>
+					</div>
+					<div class = "span3">
+						<select id = "id_localidad" name = "id_localidad" autocomplete = "off">
+							<option value = "">Localidad/Barrio</option>
+						</select>
+						<input type = "hidden" id = "localidad_text" name = "localidad_text" value = ""/>
+					</div>
+					<div class = "span3">
+					</div>					
+				</div>
+				<div class = "row">
+					<div class ="span6">
+						<label class="control-label" for="typehead">Direccion</label>	
+					</div>
+					<div class ="span1">
+						<label class="control-label" for="typehead">Piso</label>	
+					</div>
+					<div class ="span5">
+						<label class="control-label" for="typehead">Departamento</label>	
+					</div>
+				</div>
+				<div class = "row">
+					<div class = "span6">
+						<input type="text" class="span5" placeholder = "Calle" id="calle" name ="calle" value = "" maxlength= "50">
+						<input type="text" class="span1" placeholder = "Altura" id="altura" name ="altura" value = "" maxlength= "5">
+					</div>
+					<div class = "span1">
+						<select id = "piso" name="piso" class="span1">
 								<!--<option value = "">Piso</option>-->
 								<option value = "0">PB</option>
 						<?php
@@ -120,92 +157,156 @@
 						?>
 
 							</select>
-							<input type="text" class="span1" placeholder = "Depto" id="depto" name ="depto" value = "" maxlength="2">
+					</div>
+					<div class = "span5">
+						<input type="text" class="span1" placeholder = "Depto" id="depto" name ="depto" value = "" maxlength="2">
+					</div>
+				</div>
+
+				<div class = "row">
+					<div class = "span3">
+						<label class="control-label" for="typehead">Superficie cubierta</label>
+						<input type="text" class="span1" placeholder = "0" id="superficie_cubierta" name ="superficie_cubierta" value = "" maxlength= "4">m<sup>2</sup>
+					</div>
+					<div class = "span3">
+						<label class="control-label" for="typehead">Superficie Descubierta</label>
+						<input type="text" class="span1" placeholder = "0" id="superficie_descubierta" name ="superficie_descubierta" value = "" maxlength= "4"> m<sup>2</sup>
+					</div>
+					<div class = "span3">
+						<label class="control-label" for="typehead">Antiguedad</label>
+						<input type="text" style="width:25px;" placeholder = "0" id = "antiguedad" name = "antiguedad" value = "" maxlength= "3"> años
+					</div>
+					<div class = "span3">
+						
+					</div>
+				</div>
+				<div class = "row">
+					<div class = "span12">
+						<label class="control-label" for="typehead">Precio</label>
+						<select id = "moneda" name = "moneda" class="span1">
+							<option value = "$" >$</option>
+							<option value = "U$S" >U$S</option>
+						</select>
+						<input type="text" class="span1" id="precio" name ="precio" value = "" maxlength="8">
+					</div>
+				</div>
+
+				<div class = "row">
+					<div class = "span12">
+						<label class="control-label" for="typehead">Descripcion</label>
+						<textarea rows = "5" style="width:100%;" id="descripcion" name ="descripcion" maxlength="2000"></textarea>
+					</div>
+				</div>
+
+				<div class = "row">
+					<div class = "span12">
+						<div class="widget-head">
+							<h5>Ambientes</h5>
 						</div>
 					</div>
 				</div>
-				<div class="control-group">
-					<label class="control-label" for="typehead">Ambientes</label>
-					<div class="controls">
-						<div>
-							<?php
-								foreach($ambientes as $a){
-							?>
-								<label><input type = "checkbox" id = "ambientes[]" name = "ambientes[]" value = "<?php echo $a->id; ?>" /> <?php echo $a->nombre; ?></label>
-							<?php
-								}
-							?>
-							
+
+				<?php
+					$cols = 0;
+					foreach($ambientes as $a){
+						if($cols == 0){
+							echo "<div class = 'row'>";
+						}
+					?>
+						<div class = "span2">
+							<label>
+								<input type = "checkbox" id = "ambientes[]" name = "ambientes[]" value = "<?php echo $a->id; ?>" /> <?php echo $a->nombre; ?>
+							</label>
+						</div>
+					<?php
+
+						if($cols == 12){
+							echo "</div>";
+							$cols = 0;
+						}else{
+							$cols++;
+						}
+					}
+					$colsFaltantes = 0;
+					if($cols < 12){
+						$colsFaltantes = 12 - $cols;
+						echo "<div class = 'span".$colsFaltantes."'></div></div>";
+					}
+				?>
+
+				<div class = "row">
+					<div class = "span12">
+						<div class="widget-head">
+							<h5>Instalaciones</h5>
 						</div>
 					</div>
 				</div>
-				<div class="control-group">
-					<label class="control-label" for="typehead">Instalaciones</label>
-					<div class="controls">
-						<div>
-							<?php
-								foreach($instalaciones as $i){
-							?>
-								<label><input type = "checkbox" id = "instalaciones[]" name = "instalaciones[]" value = "<?php echo $i->id; ?>" /> <?php echo $i->nombre; ?></label>
-							<?php
-								}
-							?>
-							
+
+				<?php
+					$cols = 0;
+					foreach($instalaciones as $i){
+						if($cols == 0){
+							echo "<div class = 'row'>";
+						}
+					?>
+					<div class = "span2">
+						<label>
+							<input type = "checkbox" id = "instalaciones[]" name = "instalaciones[]" value = "<?php echo $i->id; ?>" /> <?php echo $i->nombre; ?></label>
+						</label>
+					</div>
+				<?php
+
+						if($cols == 12){
+							echo "</div>";
+							$cols = 0;
+						}else{
+							$cols++;
+						}
+					}
+					$colsFaltantes = 0;
+					if($cols < 12){
+						$colsFaltantes = 12 - $cols;
+						echo "<div class = 'span".$colsFaltantes."'></div></div>";
+					}
+				?>
+
+
+				<div class = "row">
+					<div class = "span12">
+						<div class="widget-head">
+							<h5>servicios</h5>
 						</div>
 					</div>
 				</div>
-				<div class="control-group">
-					<label class="control-label" for="typehead">Servicios</label>
-					<div class="controls">
-						<div>
-							<?php
-								foreach($servicios as $s){
-							?>
-								<label><input type = "checkbox" id = "servicios[]" name = "servicios[]" value = "<?php echo $s->id; ?>" /> <?php echo $s->nombre; ?></label>
-							<?php
-								}
-							?>
-							
-						</div>
+
+				<?php
+					$cols = 0;
+					foreach($servicios as $s){
+						if($cols == 0){
+							echo "<div class = 'row'>";
+						}
+					?>
+					<div class = "span2">
+						<label>
+							<input type = "checkbox" id = "servicios[]" name = "servicios[]" value = "<?php echo $s->id; ?>" /> <?php echo $s->nombre; ?>
+						</label>
 					</div>
-				</div>
-				<div class="control-group">
-					<label class="control-label" for="typehead">Superficie</label>
-					<div class="controls">
-						<div>
-							Cubierta: <input type="text" class="span1" placeholder = "0" id="superficie_cubierta" name ="superficie_cubierta" value = "" maxlength= "4">m2 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-							Descubierta: <input type="text" class="span1" placeholder = "0" id="superficie_descubierta" name ="superficie_descubierta" value = "" maxlength= "4"> m2
-						</div>
-					</div>
-				</div>
-				<div class="control-group">
-					<label class="control-label" for="typehead">Antiguedad</label>
-					<div class="controls">
-						<div>
-							<input type="text" style="width:25px;" placeholder = "0" id = "antiguedad" name = "antiguedad" value = "" maxlength= "3"> años
-						</div>
-					</div>
-				</div>
-				<div class="control-group">
-					<label class="control-label" for="typehead">Descripcion</label>
-					<div class="controls">
-						<div>
-							<textarea rows = "5" style="width:100%;" id="descripcion" name ="descripcion" maxlength="2000"></textarea>
-						</div>
-					</div>
-				</div>
-				<div class="control-group">
-					<label class="control-label" for="typehead">Precio</label>
-					<div class="controls">
-						<div>
-							<select id = "moneda" name = "moneda" class="span1">
-								<option value = "$" >$</option>
-								<option value = "U$S" >U$S</option>
-							</select>
-							<input type="text" class="span1" id="precio" name ="precio" value = "" maxlength="8">
-						</div>
-					</div>
-				</div>
+				<?php
+
+						if($cols == 12){
+							echo "</div>";
+							$cols = 0;
+						}else{
+							$cols++;
+						}
+					}
+					$colsFaltantes = 0;
+					if($cols < 12){
+						$colsFaltantes = 12 - $cols;
+						echo "<div class = 'span".$colsFaltantes."'></div></div>";
+					}
+				?>
+
 				<div class="control-group">
 					<label class="control-label">Contacto</label>
 					<div class="controls">
